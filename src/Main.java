6438d6660546124a7b5683a8e46fe63cd7ce073a
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:39:02 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Market market = new Market();
+        Human human1 = new Human("1");
+        Human human2 = new Human("2");
+        Human human3 = new Human("3");
+        Human human4 = new Human("4");
+        market.acceptToMarket(human1);
+        market.acceptToMarket(human2);
+        market.acceptToMarket(human3);
+        market.acceptToMarket(human4);
+        market.update();
     }
 }
