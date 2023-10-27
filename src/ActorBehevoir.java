32fa462a8238814e8bd355fe42594ded0106677f
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:38:17 +0500

    Update ActorBehevoir.java

--- src/ActorBehevoir.java
+++ src/ActorBehevoir.java
@@ -1,2 +1,7 @@
 package PACKAGE_NAME;public class ActorBehevoir {
+   void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean takeOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+
 }
