9be5c69b66283124529e78161edd3e63cef087cb
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2023-10-27 21:37:59 +0500

    Update Actor.java

--- src/Actor.java
+++ src/Actor.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Actor {
+package PACKAGE_NAME;public class Actor { 
+  protected String name;
+    protected boolean isMakeOrder;
+    protected boolean isTakeOrder;
+    public Actor(String name){
+        this.name = name;
+    }
+    public abstract String getName();
 }
